603c13a34d48130a828365a8f32f2fc6a20350c3
RobinShannon <shannon.rj@googlemail.com>
2020-07-24 14:24:47 +0100

    removing multiprocess

--- RunSelector.py
+++ RunSelector.py
@@ -3,6 +3,13 @@ import Globals as g
 import Main as m
 import GenBXDMain as gm
 import DosMain  as dm
+import scine_sparrow
+
+calculation = scine_sparrow.Calculation('AM1')
+calculation.set_elements(['H', 'H'])
+calculation.set_positions([[0, 0, 0], [1, 0, 0]])
+ene = calculation.calculate_energy()
+print(str(ene))
 
 
 path = os.getcwd()
