d1edff2b6bdb1e8181106a6f4fd7347e67cf167c
RobinShannon <shannon.rj@googlemail.com>
2021-01-05 11:25:54 +0000

    Added try except block free energy printing of average energies

--- ChemDyME_gauss.py
+++ ChemDyME_gauss.py
@@ -154,13 +154,13 @@ class Gaussian(FileIOCalculator):
 
     def minimise_stable(self, path = os.getcwd(), atoms: Optional[Atoms] = None):
         opt = GaussianOptimizer(atoms, self)
-        opt.run(steps=100, opt='calcall')
+        opt.run(steps=100, opt='calcall cartesian')
 
 
 
     def minimise_ts_only(self, atoms):
         opt = GaussianOptimizer(atoms, self)
-        opt.run(steps=100, opt='calcall, ts, noeigentest')
+        opt.run(steps=100, opt='calcall, ts, noeigentest, cartesian')
 
     def read_vibs(self):
         vibs = []
